a4260e8203f94ef6acbc94986ca864fee8373531
King Pacaya <83373501+king-pacaya@users.noreply.github.com>
2025-05-16 09:48:55 -0500

    Delete chifa-wuang-choy directory

--- chifa-wuang-choy/paginas/validacion.js
+++
@@ -1,6 +0,0 @@
-// Verificar si se ha cerrado sesión
-var loggedIn = localStorage.getItem("loggedIn");
-if (loggedIn === "false") {
-  // Redireccionar al login si se ha cerrado sesión
-  window.location.href = "../../login.html";
-}
